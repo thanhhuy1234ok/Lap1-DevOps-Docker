791a043809e22caa51f1b45faaf7d5bb8fadedb2
thanhhuy1234ok <nguyenvothanhhuy2002@gmail.com>
2024-12-27 18:40:18 +0700

    docker file

--- backend/index.js
+++ backend/index.js
@@ -1,104 +1,46 @@
-// const express = require('express')
-// const mongoose = - require('mongoose');
-// const cors = require('cors');
-// const app = express()
-
-// app.use(express.json())
-// app.use(cors())
-
-// const productSchema = new mongoose.Schema({
-//     name: String,
-//     price: Number
-
-// })
-
-// const ProductModel = mongoose.model('Product', productSchema)
-// mongoose.connect('mongodb://127.0.0.1:27017/test')
-//     .then(() => console.log('Connected!'));
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World')
-// })
-
-// app.post('/', (req, res) => {
-//     const product = new ProductModel(req.body)
-//     product.save()
-// })
-
-// app.get("/api/v1/products", (req, res) => {
-//     ProductModel.find().then((products) => {
-//         res.json(products)
-//     })
-// })
-
-// app.get('/detail/:id', (req, res) => {
-//     ProductModel.findById(req.params.id).then((product) => {
-//         res.json(product)
-//     })
-// })
-// app.listen(
-//     process.env.PORT || 3000,
-//     () => console.log('Server is running on port 3000')
-// )
-
-
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose');
-const app = express();
+const cors = require('cors');
+const app = express()
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cors())
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
-const userSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name: String,
-    age: Number,
-    email: String,
-});
-
-const User = mongoose.model('User', userSchema);
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.post('/addUser', async (req, res) => {
-    const isUserExists = await User.findOne({
-        email:
-            req.body.email
-    });
-
-    if (isUserExists) {
-        return res.status(400).send('User already exists');
-    }
-
-    const user = new User(req.body);
-    user.save().then(() => {
-        res.send('User added');
-    }).catch(err => {
-        res.status(400).send('Unable to save user');
-    });
-});
-
-app.listen(3001, () => {
-    console.log('Server is running on port 3000');
+    price: Number
 })
-const axios = require('axios');
 
+const ProductModel = mongoose.model('Product', productSchema)
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+    
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
-for (let i = 0; i < 1000; i++) {
-    axios.post('https://backend.trothalo.click/api/v1/resendCode',  {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+app.post('/api/v1/products', (req, res) => {
+    const product = new ProductModel(req.body)
+    product.save()
+})
+
+app.get("/api/v1/products", (req, res) => {
+    ProductModel.find().then((products) => {
+        res.json(products)
     })
-        .then(response => {
-            console.log(`User added: ${response.data}`);
-        })
-        .catch(error => {
-            console.error(`Error adding user: ${error.response ? error.response.data : error.message}`);
-        });
-}
+})
 
+app.get('/api/v1/products/:id', (req, res) => {
+    ProductModel.findById(req.params.id).then((product) => {
+        res.json(product)
+    })
+})
+app.listen(
+    PORT,
+    () => console.log(`Server is running on port ${PORT}`)
+)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ShowProduct/>
+      <AddProduct/>
     </>
+  )
+}
+
+const ShowProduct = () =>{
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = () =>{
+      const data = axios.get('http://localhost:8080/api/v1/products')
+      console.log(data);
+      return data
+    }
+    useEffect(() => {
+        fetchProducts().then((response) => {
+            setProducts(response.data)
+        })
+    }, [])
+
+    return(
+      <div>
+        <h1>Products</h1>
+        {
+          products.length > 0 ? (
+            <div>
+              {
+                products.map((product) => (
+                  <div key={product._id}>
+                    <h2>Name product: {product.name}</h2>
+                    <p>Price: {product.price}</p>
+                  </div>
+                ))
+              }   
+            </div>
+          ):(
+            <p>No products found</p>
+          )
+        }
+      </div>
+    )
+}
+
+const AddProduct = () =>{
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const addProduct = () =>{
+    axios.post('http://localhost:8080/api/v1/products', {
+      name: name,
+      price: price
+    })
+    window.location.reload()
+  }
+  return(
+    <div>
+    <h1>Add Product</h1>
+    <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
+    <input type="number" placeholder="Price" onChange={(e) => setPrice(e.target.value)}/>
+    <button onClick={addProduct}>Add Product</button>
+  </div>
   )
 }
 
